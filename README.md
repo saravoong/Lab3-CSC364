928410473c400797599831fccd4044431796e4e6
Sara V <lovechameleon@gmail.com>
2025-08-23 23:44:42 -0700

    lab 3 - counting payment types

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Lab3-CSC364
+
+Dataset: NYC Yellow Taxi Trip Data
+
+Link: https://www.kaggle.com/datasets/elemento/nyc-yellow-taxi-trip-data
+
+To start this lab:
+1) Run: **stop-all.sh** inside of home terminal
+2) Run: **start-all.sh**
+3) Download the dataset above and place it into your Desktop, and rename it (ex/ dataset.csv)
+4) Run: **cd Desktop**
+5) Run: **hadoop fs -mkdir /lab3_input**
+6) Run: **hadoop fs -put dataset.csv /lab3_input**
+7) Double check that the dataset is in Hadoop (i.e http://localhost:9870 -> Utilities -> Browse the file system)
+
+To run this lab:
+0) Make sure to follow all the steps above first
+1) Make a local copy of this repo (if there are any issues, click on the Maven tab on the right hand & click on the refresh button - in IntelliJ)
+1) Assuming you are using IntelliJ, click on the Maven tab on the right hand
+2) Click on the _Execute Maven Goal_ icon 
+3) Click **clean** and enter
+4) Click **install** and enter
+5) Run: **hadoop jar target/Lab3-1.0-SNAPSHOT.jar org.example.Lab3 /lab3_input/dataset.csv /lab3_output** inside the project terminal (i.e. inside of IntelliJ). Make sure the pathways match your local project, mines might be different.
+6) Wait until the map and reduce jobs are done, then check the output in Hadoop (i.e. check part_00000)
+7) If you ever need to restart, you can delete the output file (**hdfs dfs -rm -r /lab3_output**), maven clean, maven install, then run #5 again 
---
+++ src/main/java/org/example/Lab3.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Lab3 {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text payment_type = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String line = value.toString().trim();
+            if (line.startsWith("VendorID")) {
+                return; // skip the first line aka the header
+            }
+            String[] parts = line.split(",", -1);
+            payment_type.set(parts[11]);
+            context.write(payment_type, one);
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "count of payment type");
+        job.setJarByClass(Lab3.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
